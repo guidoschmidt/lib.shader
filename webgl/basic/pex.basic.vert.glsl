7802b42b547eb0c0093164535154a2416646293d
Guido Schmidt <git@guidoschmidt.cc>
2020-08-16 01:18:35 +0200

    EDIT: update pex shaders

--- webgl/basic/pex.basic.vert.glsl
+++ webgl/basic/pex.basic.vert.glsl
@@ -1,12 +1,5 @@
 attribute vec3 aPosition;
-attribute vec3 aNormal;
-
-uniform mat4 uProjectionMatrix;
-uniform mat4 uViewMatrix;
-
-varying vec3 vNormal;
 
 void main () {
-  gl_Position = uProjectionMatrix * uViewMatrix * vec4(aPosition, 1.0);
-  vNormal = aNormal;
+  gl_Position = vec4(aPosition, 1.0);
 }
